31ec24e4aa6733d8085097f14e346e225ade7b2a
kotook <85958338+kotook@users.noreply.github.com>
2022-06-03 12:08:13 +0300

    Update hw1.2.go

--- hw1/hw1.2.go
+++ hw1/hw1.2.go
@@ -9,8 +9,8 @@ func main() {
 	mu := sync.Mutex{}
 	wg := sync.WaitGroup{}
 	resultPath := dir.Dir()
-	wg.Add(100000)
-	for i := 0; i < 100000; i++ {
+	wg.Add(10000)
+	for i := 0; i < 10000; i++ {
 
 		go func(i int) {
 			mu.Lock()
